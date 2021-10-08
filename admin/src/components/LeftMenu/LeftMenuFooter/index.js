85f1aca6204663cca81932733d42f5f78e878b5c
Vinicius Ribeiro <vinicius.ribeiro@adpmnet.com.br>
2021-10-08 17:15:42 -0300

    Left menu e home

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,41 @@
+/**
+ *
+ * LeftMenuFooter
+ *
+ */
+
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import Wrapper, { A } from './Wrapper';
+
+function LeftMenuFooter({ version }) {
+  // PROJECT_TYPE is an env variable defined in the webpack config
+  // eslint-disable-next-line no-undef
+  const projectType = PROJECT_TYPE;
+
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <A key="website" href="https://github.com/vinicius-ribeiro" target="_blank" rel="noopener noreferrer">
+          React Vinícius Ribeiro
+        </A>
+        &nbsp;
+        <A
+          href={`https://github.com/strapi/strapi/releases/tag/v${version}`}
+          key="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          v{version}
+        </A>
+        
+      </div>
+    </Wrapper>
+  );
+}
+
+LeftMenuFooter.propTypes = {
+  version: PropTypes.string.isRequired,
+};
+
+export default LeftMenuFooter;
