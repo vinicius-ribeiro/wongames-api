d7abd88fe2be2bb9d9b81e72cf3658bb042e545d
Vinicius Ribeiro <vinicius.ribeiro@adpmnet.com.br>
2021-10-08 17:53:10 -0300

    Criacao de pagina home do plugin repositories

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,55 @@
-/*
- *
- * HomePage
- *
- */
+import React, { useState, useEffect, memo } from 'react';
+import { Header } from "@buffetjs/custom";
+import { Table } from '@buffetjs/core';
+import axios from "axios";
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import styled from "styled-components";
+
+
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`
 
 const HomePage = () => {
+  const headers = [
+    {
+      name: 'Name',
+      value: 'name',
+    },
+    {
+      name: 'Description',
+      value: 'description',
+    },
+    {
+      name: 'Url',
+      value: 'html_url',
+    },    
+  ];
+  
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/vinicius-ribeiro/repos")
+      .then(res => setRows(res.data))
+      .catch((e) => 
+        strapi.notification.error(`Limite de 60 chamadas atingido, ${e}`)
+      );
+  }, []);
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Header
+        title={{ label: "Vinícius Ribeiro - Repositories" }}
+        content="Lista de repositórios do desenvolvedor Vinícius Ribeiro"
+      />
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
   );
 };
 
